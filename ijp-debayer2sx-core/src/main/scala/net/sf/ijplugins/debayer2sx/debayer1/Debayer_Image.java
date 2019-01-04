41129699dd34d32050f211d0af9995f1417120b0
Jarek Sacha <jpsacha@gmail.com>
2019-01-03 19:12:58 -0500

    Correct copyright and information about Debayer1.

--- ijp-debayer2sx-core/src/main/scala/net/sf/ijplugins/debayer2sx/debayer1/Debayer_Image.java
+++ ijp-debayer2sx-core/src/main/scala/net/sf/ijplugins/debayer2sx/debayer1/Debayer_Image.java
@@ -1,23 +1,5 @@
 /*
- * Image/J Plugins
- * Copyright (C) 2002-2018 Jarek Sacha
- * Author's email: jpsacha at gmail [dot] com
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- *
- * Latest release available at http://sourceforge.net/projects/ij-plugins/
+ * Copyright Jennifer West (?)
  */
 package net.sf.ijplugins.debayer2sx.debayer1;
 
@@ -29,7 +11,10 @@ import ij.plugin.filter.PlugInFilter;
 import ij.process.ImageProcessor;
 import ij.process.ShortProcessor;
 
-
+/**
+ * Debayer ImageJ plugin provide by Jennifer West from University of Manitoba in 2006.
+ * http://umanitoba.ca/faculties/science/astronomy/jwest/plugins.html
+ */
 @SuppressWarnings("ALL")
 public class Debayer_Image implements PlugInFilter {
 
